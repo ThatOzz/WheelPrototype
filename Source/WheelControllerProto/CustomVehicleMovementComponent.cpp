a5e0677a4104f8f26eb57d3ea139f8faa8e57a2a
ThatOzz <ozzugaming@gmail.com>
2025-04-25 17:59:21 +0300

    Original code commits

---
+++ Source/WheelControllerProto/CustomVehicleMovementComponent.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomVehicleMovementComponent.h"
+
+CustomVehicleMovementComponent::CustomVehicleMovementComponent()
+{
+}
+
+CustomVehicleMovementComponent::~CustomVehicleMovementComponent()
+{
+}
