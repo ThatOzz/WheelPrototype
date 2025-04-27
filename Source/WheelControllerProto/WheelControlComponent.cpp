2d9453b62f113b667544a413e581f5b3e95a4f8f
ThatOzz <ozzugaming@gmail.com>
2025-04-27 23:21:56 +0300

    Fixes

--- Source/WheelControllerProto/WheelControlComponent.cpp
+++ Source/WheelControllerProto/WheelControlComponent.cpp
@@ -1,6 +1,7 @@
 #include "WheelControlComponent.h"
 #include "CustomVehicleMovementController.h"
 #include "GameFramework/Actor.h"
+#include "Math/UnrealMathUtility.h"
 
 UWheelControlComponent::UWheelControlComponent()
 {
@@ -21,22 +22,48 @@ void UWheelControlComponent::BeginPlay()
 void UWheelControlComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-    if (!MovementComponent) return;
-
-    UE_LOG(LogTemp, Log, TEXT("CurrentSpeed: %f"), (MovementComponent->GetForwardSpeed() * 0.036f));
-    if ((MovementComponent->GetForwardSpeed() * 0.036f) < MaxSpeed)
+    bool overMaxSpeed = false;
+    bool skipLeft = false;
+    bool skipRight = false;
+    
+    //Check if over max speed
+    if (abs(MovementComponent->GetForwardSpeed() * 0.036f) > MaxSpeed) //Check for max speed
     {
-        MovementComponent->SetDriveTorquePerWheel(1, LeftInput * MaxTorque);  // Front Left
-        MovementComponent->SetDriveTorquePerWheel(3, LeftInput * MaxTorque);  // Rear Left
-        MovementComponent->SetDriveTorquePerWheel(0, RightInput * MaxTorque); // Front Right
-        MovementComponent->SetDriveTorquePerWheel(2, RightInput * MaxTorque); // Rear Right
+        overMaxSpeed = true;
     }
-    else
+    
+    //Left Throttle
+    if ((MovementComponent->GetForwardSpeed()*LeftInput) > 0) //Check if accelerating or decelerating
     {
-        for (int i = 0; i < 4; i++)
+        if (overMaxSpeed) //Check if already at max speed (Forwards and Backwards)
         {
-            MovementComponent->SetDriveTorquePerWheel(i, 0);
+            LeftInput = 0;
+            MovementComponent->SetDriveTorquePerWheel(0, LeftInput * MaxTorque);  // Front Left
+            MovementComponent->SetDriveTorquePerWheel(2, LeftInput * MaxTorque);  // Rear Left
+            skipLeft = true;
         }
+    }
+    
+    //Right Throttle
+    if ((MovementComponent->GetForwardSpeed() * RightInput) > 0) //Check if accelerating or decelerating
+    {
+        if (overMaxSpeed) //Check if already at max speed (Forwards and Backwards)
+        {
+            RightInput = 0;
+            MovementComponent->SetDriveTorquePerWheel(1, RightInput * MaxTorque);  // Front Right
+            MovementComponent->SetDriveTorquePerWheel(3, RightInput * MaxTorque); // Rear Right
+            skipRight = true;
+        }
+    }
+
+    if (!skipLeft)
+    {
+        MovementComponent->SetDriveTorquePerWheel(0, LeftInput * MaxTorque);  // Front Left
+        MovementComponent->SetDriveTorquePerWheel(2, LeftInput * MaxTorque);  // Rear Left
+    }
+    if (!skipRight)
+    {
+        MovementComponent->SetDriveTorquePerWheel(1, RightInput * MaxTorque); // Front Right
+        MovementComponent->SetDriveTorquePerWheel(3, RightInput * MaxTorque); // Rear Right
     }
 }
